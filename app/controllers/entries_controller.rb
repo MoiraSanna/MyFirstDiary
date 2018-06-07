41f9284a6bff5a3e3c3302ce27b7cd6123e79c4d
moira <moira.sanna@gmail.com>
2018-06-07 11:15:24 +0100

    Created our diary entries views and defined actions

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,15 +1,25 @@
 class EntriesController < ApplicationController
+  before_action :find_entry, only: [:show, :edit, :update, :destroy]
 
   def index
+  	@entries = Entry.all.order("created_at DESC")
   end
   
   def show
   end
 
   def new
+  	@entry = Entry.new
   end
 
   def create
+  	@entry = Entry.new(entry_params)
+
+  	if @entry.save
+  	  redirect_to @entry
+    else 
+  	  render 'new'
+  	end    
   end
 
   def edit
@@ -24,8 +34,10 @@ class EntriesController < ApplicationController
   private
 
     def find_entry
+      @entry = Entry.find(params[:id])	
     end
 
     def entry_params
+      params.require(:entry).permit(:title, :content)
     end	
 end
