30e7dad2ecc38a48a55fbb451674ffe972269db5
moira <moira.sanna@gmail.com>
2018-06-09 12:24:10 +0100

    Configured route for authenticated users

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
   
-  root 'welcome#index'
-
   resources :entries
+  authenticated :user do
+  	root "entries#index", as: "authenticated_root"
+  end	
+
+  root 'welcome#index'
 end
